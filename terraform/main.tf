0949be8fab0446c3676af1379fa432150123c59e
Jeremy Guillette <3651377+jaguillette@users.noreply.github.com>
2023-06-30 17:53:42 -0400

    update requirements and terraform to be aware of file changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_lambda_layer_version" "canvas" {
+data "local_file" "lambda_layer" {
   filename = "../canvasLayer.zip"
+}
+
+resource "aws_lambda_layer_version" "canvas" {
+  filename = data.local_file.lambda_layer.filename
   layer_name = "canvasLambda"
   compatible_runtimes = ["python3.9"]
+
+  source_code_hash = data.local_file.lambda_layer.content_base64sha256
 }
